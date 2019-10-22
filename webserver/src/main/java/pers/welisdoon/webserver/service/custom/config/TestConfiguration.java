cbea46bb314e6d5c0388dd480cccfe3af0c1f3f1
Wang ZhiDong <6027021846@iwhalecloud.com>
2019-10-22 22:04:03 +0800

    页面

--- webserver/src/main/java/pers/welisdoon/webserver/service/custom/config/TestConfiguration.java
+++ webserver/src/main/java/pers/welisdoon/webserver/service/custom/config/TestConfiguration.java
@@ -1,21 +1,154 @@
 package pers.welisdoon.webserver.service.custom.config;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
+import java.util.function.Consumer;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.ibatis.executor.statement.RoutingStatementHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.FileUpload;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.Session;
 import pers.welisdoon.webserver.common.config.AbstractWechatConfiguration;
+import pers.welisdoon.webserver.common.web.CommonAsynService;
+import pers.welisdoon.webserver.service.custom.service.TestService;
+import pers.welisdoon.webserver.vertx.annotation.VertxConfiguration;
+import pers.welisdoon.webserver.vertx.annotation.VertxRegister;
+import pers.welisdoon.webserver.vertx.verticle.StandaredVerticle;
 
 @Configuration
 @ConfigurationProperties("wechat-app")
+@VertxConfiguration
 public class TestConfiguration extends AbstractWechatConfiguration {
 
-    @PostConstruct
-    public void aVoid(){
-        System.out.println(this.getUrls());
+    private static final Logger logger = LoggerFactory.getLogger(TestService.class);
+
+    CommonAsynService commonAsynService;
+
+    @VertxRegister(StandaredVerticle.class)
+    public Consumer<Vertx> createAsyncServiceProxy() {
+        Consumer<Vertx> vertxConsumer = vertx1 -> {
+            commonAsynService = CommonAsynService.createProxy(vertx1);
+            System.out.println(commonAsynService);
+        };
+        return vertxConsumer;
+    }
+
+    @VertxRegister(StandaredVerticle.class)
+    public Consumer<Router> routeMapping(Vertx vertx) {
+        Consumer<Router> routerConsumer = router -> {
+            router.get("/wxApp").handler(routingContext -> {
+                routingContext.response().setChunked(true);
+                JsonArray jsonArray = new JsonArray()
+                        .add("orderManger")
+                        .add(new JsonArray()
+                                .add(1)
+                                .add(new JsonObject().put("orderId", "1")));
+                this.getPrams(jsonArray, routingContext);
+                commonAsynService.serviceCall("testService", jsonArray.getString(0), jsonArray.getJsonArray(1).toString(), jsonArray.getJsonArray(2).toString(), stringAsyncResult -> {
+                    if (stringAsyncResult.succeeded()) {
+                        routingContext.response().end(stringAsyncResult.result());
+                        System.out.println();
+                    }
+                    else {
+                        routingContext.fail(500, stringAsyncResult.cause());
+                    }
+                });
+            }).failureHandler(routingContext -> {
+                routingContext.response().end(routingContext.failure().toString());
+            });
+            router.post("/wxApp").handler(routingContext -> {
+                routingContext.response().setChunked(true);
+                JsonArray jsonArray = routingContext.getBodyAsJsonArray();
+                this.getPrams(jsonArray, routingContext);
+                commonAsynService.serviceCall("testService", jsonArray.getString(0), jsonArray.getJsonArray(1).toString(), jsonArray.getJsonArray(2).toString(), stringAsyncResult -> {
+                    if (stringAsyncResult.succeeded()) {
+                        routingContext.response().end(stringAsyncResult.result());
+                    }
+                    else {
+                        routingContext.fail(500, stringAsyncResult.cause());
+                    }
+                });
+
+            }).failureHandler(routingContext -> {
+                routingContext.response().end(routingContext.failure().toString());
+            });
+            {
+                Router subRouter = Router.router(vertx);
+                subRouter.post("/imgUpd").blockingHandler(routingContext -> {
+                    HttpServerRequest httpServerRequest = routingContext.request();
+                    Set<FileUpload> fileUploads = routingContext.fileUploads();
+                    Iterator<FileUpload> iterator = fileUploads.iterator();
+                    FileUpload fileUpload = null;
+                    while (iterator.hasNext()) {
+                        fileUpload = iterator.next();
+                        break;
+                    }
+                    File file = null;
+                    if (fileUpload != null && (file = new File(fileUpload.uploadedFileName())).exists()) {
+                        try {
+                            byte[] bytes = new FileInputStream(file).readAllBytes();
+                            String id = httpServerRequest.params().get("id");
+                            String relaId = httpServerRequest.params().get("relaId");
+                            String typeId = httpServerRequest.params().get("typeId");
+                            /*customDao.saveImage(Map.of("picture_id", id,
+                                    "picture_name", fileUpload.fileName(),
+                                    "picture_storage", bytes,
+                                    "related_id", relaId,
+                                    "related_type_id", typeId));*/
+
+                        }
+                        catch (Throwable e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                });
+                subRouter.post("/imgDl/:picType/:picId").handler(routingContext -> {
+                    String picType = routingContext.request().getParam("picType");
+                    String picId = routingContext.request().getParam("picId");
+                    routingContext.response().sendFile("");
+                });
+                router.mountSubRouter("/wxApp", subRouter);
+            }
+            logger.info("inital request mapping: /wxApp");
+        };
+        return routerConsumer;
+    }
+
+    private void getPrams(JsonArray jsonArray, RoutingContext routingContext) {
+        {
+            JsonArray arg3 = new JsonArray();
+            JsonObject jsonObject = new JsonObject();
+            {
+                Iterator<Map.Entry<String, String>> iterator = routingContext.request().params().iterator();
+                while (iterator.hasNext()) {
+                    Map.Entry<String, String> stringEntry = iterator.next();
+                    jsonObject.put(stringEntry.getKey(), stringEntry.getValue());
+                }
+                arg3.add(jsonObject);
+            }
+            {
+                Session session = routingContext.session();
+                Map<String, Object> sessionData = session != null ? session.data() : Map.of();
+                arg3.add(sessionData);
+            }
+            jsonArray.add(arg3);
+        }
     }
 }
--- webserver/src/main/java/pers/welisdoon/webserver/service/custom/service/TestService.java
+++ webserver/src/main/java/pers/welisdoon/webserver/service/custom/service/TestService.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import pers.welisdoon.webserver.common.web.CommonAsynService;
+import pers.welisdoon.webserver.service.custom.config.TestConfiguration;
 import pers.welisdoon.webserver.service.custom.dao.OrderDao;
 import pers.welisdoon.webserver.service.custom.entity.OrderVO;
 import pers.welisdoon.webserver.vertx.annotation.VertxConfiguration;
@@ -32,136 +33,53 @@ import java.util.function.Consumer;
 @Service
 @VertxConfiguration
 public class TestService {
+
+    public final int ADD = 0;
+    public final int DELETE = 1;
+    public final int MODIFY = 2;
+    public final int GET = 3;
+
     @Autowired
     OrderDao customDao;
     @Autowired
     SqlSessionTemplate sqlSessionTemplate;
-
     private static final Logger logger = LoggerFactory.getLogger(TestService.class);
-    CommonAsynService commonAsynService;
-
-    @VertxRegister(StandaredVerticle.class)
-    public Consumer<Vertx> createAsyncServiceProxy() {
-        Consumer<Vertx> vertxConsumer = vertx1 -> {
-            commonAsynService = CommonAsynService.createProxy(vertx1);
-            System.out.println(commonAsynService);
-        };
-        return vertxConsumer;
-    }
-
-    @VertxRegister(StandaredVerticle.class)
-    public Consumer<Router> routeMapping(Vertx vertx) {
-        Consumer<Router> routerConsumer = router -> {
-            router.get("/wxApp").handler(routingContext -> {
-                routingContext.response().setChunked(true);
-                JsonArray jsonArray = new JsonArray()
-                        .add("orderManger")
-                        .add(new JsonArray()
-                                .add(1)
-                                .add(new JsonObject().put("orderId", "1")));
-                {
-                    JsonArray arg3 = new JsonArray();
-                    JsonObject jsonObject = new JsonObject();
-                    {
-                        Iterator<Map.Entry<String, String>> iterator = routingContext.request().params().iterator();
-                        while (iterator.hasNext()) {
-                            Map.Entry<String, String> stringEntry = iterator.next();
-                            jsonObject.put(stringEntry.getKey(), stringEntry.getValue());
-                        }
-                        arg3.add(jsonObject);
-                    }
-                    {
-                        Session session = routingContext.session();
-                        Map<String, Object> sessionData = session != null ? session.data() : Map.of();
-                        arg3.add(sessionData);
-                    }
-                    jsonArray.add(arg3);
-                }
-                commonAsynService.serviceCall("testService", jsonArray.getString(0), jsonArray.getJsonArray(1).toString(), jsonArray.getJsonArray(2).toString(), stringAsyncResult -> {
-                    if (stringAsyncResult.succeeded()) {
-                        routingContext.response().end(stringAsyncResult.result());
-                    }
-                    else {
-                        routingContext.fail(500, stringAsyncResult.cause());
-                    }
-                });
-            }).failureHandler(routingContext -> {
-                routingContext.response().end(routingContext.failure().toString());
-            });
-            router.post("/wxApp").handler(routingContext -> {
-                routingContext.response().setChunked(true);
-                JsonArray jsonArray = routingContext.getBodyAsJsonArray();
-                commonAsynService.serviceCall("testService", jsonArray.getString(0), jsonArray.getString(1), null, stringAsyncResult -> {
-                    if (stringAsyncResult.succeeded()) {
-                        routingContext.response().end(stringAsyncResult.result());
-                    }
-                    else {
-                        routingContext.fail(500, stringAsyncResult.cause());
-                    }
-                });
-
-            }).failureHandler(routingContext -> {
-                routingContext.response().end(routingContext.failure().toString());
-            });
-            {
-                Router subRouter = Router.router(vertx);
-                subRouter.post("/imgUpd").blockingHandler(routingContext -> {
-                    HttpServerRequest httpServerRequest = routingContext.request();
-                    Set<FileUpload> fileUploads = routingContext.fileUploads();
-                    Iterator<FileUpload> iterator = fileUploads.iterator();
-                    FileUpload fileUpload = null;
-                    while (iterator.hasNext()) {
-                        fileUpload = iterator.next();
-                        break;
-                    }
-                    File file = null;
-                    if (fileUpload != null && (file = new File(fileUpload.uploadedFileName())).exists()) {
-                        try {
-                            byte[] bytes = new FileInputStream(file).readAllBytes();
-                            String id = httpServerRequest.params().get("id");
-                            String relaId = httpServerRequest.params().get("relaId");
-                            String typeId = httpServerRequest.params().get("typeId");
-                            /*customDao.saveImage(Map.of("picture_id", id,
-                                    "picture_name", fileUpload.fileName(),
-                                    "picture_storage", bytes,
-                                    "related_id", relaId,
-                                    "related_type_id", typeId));*/
-
-                        }
-                        catch (Throwable e) {
-                            e.printStackTrace();
-                        }
-                    }
-
-                });
-                subRouter.post("/imgDl/:picType/:picId").handler(routingContext -> {
-                    String picType = routingContext.request().getParam("picType");
-                    String picId = routingContext.request().getParam("picId");
-                    routingContext.response().sendFile("");
-                });
-                router.mountSubRouter("/wxApp", subRouter);
-            }
-            logger.info("inital request mapping: /wxApp");
-        };
-        return routerConsumer;
-    }
 
 
     public Object orderManger(int mode, Map params) {
-        OrderVO orderVO = JsonObject.mapFrom(params).mapTo(OrderVO.class);
-        List list = customDao.list(orderVO);
-        return Map.of("input",params,"output",list);
+        Object resultObj = null;
+        OrderVO orderVO;
+        switch (mode) {
+            case ADD:
+                orderVO = JsonObject.mapFrom(params).mapTo(OrderVO.class);
+                customDao.put(orderVO);
+                resultObj = orderVO;
+                break;
+            case DELETE:
+                break;
+            case MODIFY:
+                break;
+            case GET:
+                orderVO = JsonObject.mapFrom(params).mapTo(OrderVO.class);
+                List list = customDao.list(orderVO);
+                resultObj = list;
+                break;
+            default:
+                break;
+
+        }
+        return Map.of("input", params, "output", resultObj);
     }
 
     public Object carManger(int mode, Map params) {
         OrderVO orderVO = JsonObject.mapFrom(params).mapTo(OrderVO.class);
         List list = customDao.list(orderVO);
-        return Map.of("input",params,"output",list);
+        return Map.of("input", params, "output", list);
     }
 
     public Object userManger(int mode, Map params) {
         OrderVO orderVO = JsonObject.mapFrom(params).mapTo(OrderVO.class);
         List list = customDao.list(orderVO);
-        return Map.of("input",params,"output",list);
+        return Map.of("input", params, "output", list);
     }
 }
