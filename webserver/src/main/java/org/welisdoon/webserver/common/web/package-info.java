8008469ed06f373f2d6202d8a0e2dda3b8509b60
Wang ZhiDong <6027021846@iwhalecloud.com>
2020-01-11 22:09:30 +0800

    3.8.3

--- webserver/src/main/java/org/welisdoon/webserver/common/web/package-info.java
+++ webserver/src/main/java/org/welisdoon/webserver/common/web/package-info.java
@@ -1,4 +1,4 @@
-@ModuleGen(groupPackage = "pers.welisdoon.webserver.common.web", name = "web")
+@ModuleGen(groupPackage = "org.welisdoon.webserver.common.web", name = "web")
 
 package org.welisdoon.webserver.common.web;
 import io.vertx.codegen.annotations.ModuleGen;
