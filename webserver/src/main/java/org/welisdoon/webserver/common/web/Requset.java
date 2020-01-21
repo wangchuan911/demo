7ae10c55bbc95b2e0a9f41f7191791e177fdb728
Wang ZhiDong <6027021846@iwhalecloud.com>
2020-01-21 10:08:25 +0800

    3.8.3

--- webserver/src/main/java/org/welisdoon/webserver/common/web/Requset.java
+++ webserver/src/main/java/org/welisdoon/webserver/common/web/Requset.java
@@ -1,13 +1,18 @@
 package org.welisdoon.webserver.common.web;
 
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import io.vertx.codegen.annotations.DataObject;
 import io.vertx.codegen.annotations.GenIgnore;
 import io.vertx.core.MultiMap;
+import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.FileUpload;
+import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.Session;
 import org.springframework.util.StringUtils;
 
@@ -105,5 +110,65 @@ public class Requset {
             this.setParams(Json.encode(params));
         }
         return this;
+    }
+
+    public static final int SIMPLE_REQUEST = 2;
+    public static final int UPLOAD_FILE = 3;
+    public static final int UPLOAD_FILES = 4;
+
+    public static Requset newInstance(int mode, RoutingContext context) {
+        HttpServerRequest httpServerRequest = context.request();
+        Requset requset = new Requset();
+        String body = null;
+        String method = null;
+        switch (mode) {
+            case SIMPLE_REQUEST: {
+                JsonArray jsonArray = context.getBodyAsJsonArray();
+                method = jsonArray.getString(0);
+                body = jsonArray.getJsonArray(1).toString();
+            }
+            break;
+            case UPLOAD_FILES: {
+                Set<FileUpload> fileUploads = context.fileUploads();
+                method = "uploadFiles";
+                body = new JsonArray()
+                        .add(fileUploads.stream().map(Requset::loadFileToJson).collect(Collectors.toList()))
+                        .add(formToJson(httpServerRequest)).toString();
+            }
+            break;
+            case UPLOAD_FILE: {
+                Set<FileUpload> fileUploads = context.fileUploads();
+                FileUpload fileUpload = fileUploads.iterator().next();
+                method = "uploadFile";
+                body = (new JsonArray()
+                        .add(loadFileToJson(fileUpload))
+                        .add(formToJson(httpServerRequest)).toString());
+
+            }
+            break;
+        }
+
+        requset.setMethod(method).setBody(body).putParams(httpServerRequest.params())
+                .putSession(context.session());
+        return requset;
+    }
+
+    static JsonObject loadFileToJson(FileUpload fileUpload) {
+        return new JsonObject()
+                .put("uploadedFileName", fileUpload.uploadedFileName())
+                .put("name", fileUpload.name())
+                .put("charSet", fileUpload.charSet())
+                .put("contentType", fileUpload.contentType())
+                .put("size", fileUpload.size())
+                .put("fileName", fileUpload.fileName())
+                .put("contentTransferEncoding", fileUpload.contentTransferEncoding());
+    }
+
+    static JsonObject formToJson(HttpServerRequest httpServerRequest) {
+        return new JsonObject(httpServerRequest
+                .formAttributes()
+                .entries()
+                .stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
     }
 }
