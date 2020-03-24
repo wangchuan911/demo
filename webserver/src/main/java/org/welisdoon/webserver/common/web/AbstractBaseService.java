c7bf1ae530365d322ab17f08cc4607821c6b45f6
Wang ZhiDong <6027021846@iwhalecloud.com>
2020-03-24 20:17:27 +0800

    3.8.3

--- webserver/src/main/java/org/welisdoon/webserver/common/web/AbstractBaseService.java
+++ webserver/src/main/java/org/welisdoon/webserver/common/web/AbstractBaseService.java
@@ -1,10 +1,14 @@
 package org.welisdoon.webserver.common.web;
 
 import io.vertx.core.json.JsonObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.welisdoon.webserver.service.custom.config.CustomConst;
 import org.welisdoon.webserver.service.custom.entity.TacheVO;
 
 import javax.annotation.PostConstruct;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -24,4 +28,31 @@ public abstract class AbstractBaseService<T> {
         return JsonObject.mapFrom(params).mapTo(type);
     }
 
+    public static <T> void removeMapKeys(Map<T, ?> map, T... keys) {
+        Arrays.stream(keys).forEach(t -> map.remove(t));
+    }
+
+    public static <T> Map<T, ?> mapSpliter(Map<T, ?> map, Class<? extends Map> targetMapClass, T... keys) {
+        int len;
+        if ((len = keys.length) == 0) return null;
+        if (targetMapClass == Map.class) {
+            Map.Entry<T, ?>[] entries = Arrays.stream(keys)
+                    .filter(key -> map.get(key) != null)
+                    .map(key -> Map.entry(key, map.remove(key)))
+                    .toArray(Map.Entry[]::new);
+            return Map.ofEntries(entries);
+        } else {
+            T key;
+            Map<T, Object> hashMap = new HashMap<>(len + 1, 1.0f);
+            for (int i = 0; i < len; i++) {
+                key = keys[i];
+                hashMap.put(key, map.remove(key));
+            }
+            return hashMap;
+        }
+    }
+
+    public static Logger logger(Class<?> type) {
+        return LoggerFactory.getLogger(type);
+    }
 }
