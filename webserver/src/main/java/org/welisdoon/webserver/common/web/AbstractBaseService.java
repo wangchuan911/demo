d7e1885708a8ce524b27348181125c64c5f66989
Wang ZhiDong <6027021846@iwhalecloud.com>
2020-06-24 20:26:09 +0800

    3.8.3

--- webserver/src/main/java/org/welisdoon/webserver/common/web/AbstractBaseService.java
+++ webserver/src/main/java/org/welisdoon/webserver/common/web/AbstractBaseService.java
@@ -12,45 +12,49 @@ import java.util.Map;
 
 public abstract class AbstractBaseService<T> {
 
-    public abstract Object handle(int exeCode, Map map);
+	final public Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    public Object handle(int exeCode, T map) {
-        throw new RuntimeException();
-    }
+	public abstract Object handle(int exeCode, Map map);
 
-    @PostConstruct
-    public void init() throws Throwable {
-    }
+	public Object handle(int exeCode, T map) {
+		throw new RuntimeException();
+	}
 
-    public static <K> K mapToObject(Map params, Class<K> type) {
-        return JsonObject.mapFrom(params).mapTo(type);
-    }
+	@PostConstruct
+	public void init() throws Throwable {
 
-    public static <T> void removeMapKeys(Map<T, ?> map, T... keys) {
-        Arrays.stream(keys).forEach(t -> map.remove(t));
-    }
+	}
 
-    public static <T> Map<T, ?> mapSpliter(Map<T, ?> map, Class<? extends Map> targetMapClass, T... keys) {
-        int len;
-        if ((len = keys.length) == 0) return null;
-        if (targetMapClass == Map.class) {
-            Map.Entry<T, ?>[] entries = Arrays.stream(keys)
-                    .filter(key -> map.get(key) != null)
-                    .map(key -> Map.entry(key, map.remove(key)))
-                    .toArray(Map.Entry[]::new);
-            return Map.ofEntries(entries);
-        } else {
-            T key;
-            Map<T, Object> hashMap = new HashMap<>(len + 1, 1.0f);
-            for (int i = 0; i < len; i++) {
-                key = keys[i];
-                hashMap.put(key, map.remove(key));
-            }
-            return hashMap;
-        }
-    }
+	public static <K> K mapToObject(Map params, Class<K> type) {
+		return JsonObject.mapFrom(params).mapTo(type);
+	}
 
-    public static Logger logger(Class<?> type) {
-        return LoggerFactory.getLogger(type);
-    }
+	public static <T> void removeMapKeys(Map<T, ?> map, T... keys) {
+		Arrays.stream(keys).forEach(t -> map.remove(t));
+	}
+
+	public static <T> Map<T, ?> mapSpliter(Map<T, ?> map, Class<? extends Map> targetMapClass, T... keys) {
+		int len;
+		if ((len = keys.length) == 0) return null;
+		if (targetMapClass == Map.class) {
+			Map.Entry<T, ?>[] entries = Arrays.stream(keys)
+					.filter(key -> map.get(key) != null)
+					.map(key -> Map.entry(key, map.remove(key)))
+					.toArray(Map.Entry[]::new);
+			return Map.ofEntries(entries);
+		} else {
+			T key;
+			Map<T, Object> hashMap = new HashMap<>(len + 1, 1.0f);
+			for (int i = 0; i < len; i++) {
+				key = keys[i];
+				hashMap.put(key, map.remove(key));
+			}
+			return hashMap;
+		}
+	}
+
+	public static Logger logger(Class<?> type) {
+		return LoggerFactory.getLogger(type);
+	}
+
 }
