fd41b980ded1f9e261ae39c305124241ba12cc62
Wang ZhiDong <6027021846@iwhalecloud.com>
2020-11-19 23:25:49 +0800

    3.8.3

--- webserver/src/main/java/org/welisdoon/webserver/common/EntityObjectUtils.java
+++ webserver/src/main/java/org/welisdoon/webserver/common/EntityObjectUtils.java
@@ -6,6 +6,7 @@ import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.welisdoon.webserver.common.annotation.EntityObjectKey;
+import org.welisdoon.webserver.common.annotation.EntitySpecialType;
 
 import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Field;
@@ -15,33 +16,39 @@ import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public interface EntityObjectUtils {
     Logger logger = LoggerFactory.getLogger(EntityObjectUtils.class);
-    /*Map<Class<?>, Set<Field>> fieldMap = new HashMap<>();
-    Map<Class<?>, Set<Method>> methodMap = new HashMap<>();*/
-    Map<Class<?>, AccessibleObject[]> CLASS_SET_MAP = new HashMap<>();
+    Map<Class<?>, EntityObjectDeclare> CLASS_SET_MAP = new HashMap<>();
 
     static Object objValueByKey(Object t, String key) {
         Class tKey = t.getClass();
         //----------------------------------
-        AccessibleObject[] accessibleObjects = CLASS_SET_MAP.get(tKey);
-        if (accessibleObjects == null) {
+
+        if (!CLASS_SET_MAP.containsKey(tKey)) {
             synchronized (CLASS_SET_MAP) {
                 if (!CLASS_SET_MAP.containsKey(tKey)) {
-                    AccessibleObject[][] sets = new AccessibleObject[][]{
-                            ReflectionUtils.getFields(tKey, ReflectionUtils.withAnnotation(EntityObjectKey.class)).stream().toArray(AccessibleObject[]::new)
-                            , ReflectionUtils.getMethods(tKey, ReflectionUtils.withAnnotation(EntityObjectKey.class)).stream().toArray(AccessibleObject[]::new)};
-                    CLASS_SET_MAP
-                            .put(tKey, accessibleObjects = Arrays.stream(sets)
-                                    .flatMap(set -> Arrays.stream(set))
-                                    .toArray(AccessibleObject[]::new));
-                } else {
-                    accessibleObjects = CLASS_SET_MAP.get(tKey);
+                    CLASS_SET_MAP.put(tKey, new EntityObjectDeclare());
                 }
             }
         }
+        EntityObjectDeclare entityObjectDeclare = CLASS_SET_MAP.get(tKey);
+
+        if (entityObjectDeclare.annotatoionAccessibleObjects == null) {
+            synchronized (CLASS_SET_MAP) {
+                if (entityObjectDeclare.annotatoionAccessibleObjects == null) {
+                    entityObjectDeclare.annotatoionAccessibleObjects = Arrays
+                            .stream(
+                                    new AccessibleObject[][]{
+                                            ReflectionUtils.getFields(tKey, ReflectionUtils.withAnnotation(EntityObjectKey.class)).stream().toArray(AccessibleObject[]::new)
+                                            , ReflectionUtils.getMethods(tKey, ReflectionUtils.withAnnotation(EntityObjectKey.class)).stream().toArray(AccessibleObject[]::new)}
+                            )
+                            .flatMap(set -> Arrays.stream(set))
+                            .toArray(AccessibleObject[]::new);
+                }
+            }
+        }
+        AccessibleObject[] accessibleObjects = entityObjectDeclare.annotatoionAccessibleObjects;
 
         Optional<AccessibleObject> optional = Arrays
                 .stream(accessibleObjects)
@@ -50,67 +57,29 @@ public interface EntityObjectUtils {
                 .findFirst();
 
         if (optional.isPresent()) {
-            Object val = optional.get();
+            AccessibleObject val = optional.get();
             try {
-                if (val instanceof Method)
-                    return ((Method) val).invoke(t);
-                else
-                    return ((Field) val).get(t);
-
+                return getValue(val, t);
             } catch (IllegalAccessException | InvocationTargetException e) {
                 logger.error(e.getMessage(), e);
             }
         }
-        //----------------------------------
-        /*Set<Field> fields = fieldMap.get(tKey);
-        if (CollectionUtils.isEmpty(fields)) {
-            fields = ReflectionUtils.getFields(tKey, ReflectionUtils.withAnnotation(EntityObjectKey.class));
-            synchronized (fieldMap) {
-                if (fieldMap.containsKey(tKey))
-                    fieldMap.put(tKey, fields);
-            }
-        } else {
-            fields = fieldMap.get(tKey);
-        }
-        Optional<Field> optionalField = fields.stream().filter(field -> {
-            EntityObjectKey annotation = field.getAnnotation(EntityObjectKey.class);
-            return annotation != null && key.equals(annotation.name());
-        }).findFirst();
-
-        if (optionalField.isPresent()) {
-            try {
-                return optionalField.get().get(t);
-            } catch (IllegalAccessException e) {
-                logger.error(e.getMessage(), e);
+        if (entityObjectDeclare.getMethod == null) {
+            synchronized (CLASS_SET_MAP) {
+                if (entityObjectDeclare.getMethod == null) {
+                    entityObjectDeclare.getMethod = Arrays.stream(tKey.getMethods()).filter(method ->
+                            method.getName().indexOf("get") == 0 && method.getParameterCount() == 0
+                    ).toArray(Method[]::new);
+                }
             }
         }
-
-        Set<Method> methods = methodMap.get(tKey);
-        if (CollectionUtils.isEmpty(methods)) {
-            methods = ReflectionUtils.getMethods(tKey, ReflectionUtils.withAnnotation(EntityObjectKey.class));
-            synchronized (methodMap) {
-                if (methodMap.containsKey(tKey))
-                    methodMap.put(tKey, methods);
-            }
-        } else {
-            methods = methodMap.get(tKey);
-        }
-        Optional<Method> optionalMethod = methods.stream().filter(method -> {
-            EntityObjectKey annotation = method.getAnnotation(EntityObjectKey.class);
-            return annotation != null && key.equals(annotation.name());
-        }).findFirst();
-
-        if (optionalMethod.isPresent()) {
-            try {
-                return optionalMethod.get().invoke(t, key);
-            } catch (IllegalAccessException | InvocationTargetException e) {
-                logger.error(e.getMessage(), e);
-            }
-        }*/
-
+        Method[] getMethods = entityObjectDeclare.getMethod;
         try {
-            Object res = t.getClass().getMethod("get" + key).invoke(t);
-//                            logger.warn(res.getClass().getName());
+            char first = key.charAt(0);
+            String mthName = "get" + ((first >= 'a' && first <= 'z') ? (char) (first - 32) : first) + key.substring(1);
+            Optional<Method> optionalMethod = Arrays.stream(getMethods).filter(method -> method.getName().equals(mthName)).findFirst();
+            if (optionalMethod.isEmpty()) return null;
+            Object res = optionalMethod.get().invoke(t);
             if (res instanceof Date) {
                 res = LocalDateTime.ofInstant(((Date) res).toInstant(), ZoneId.systemDefault());
             } else if (res instanceof Timestamp) {
@@ -127,8 +96,6 @@ public interface EntityObjectUtils {
                         ((LocalDateTime) res).getMinute()));
             }
             return res != null ? res.toString() : "";
-        } catch (NoSuchMethodException e) {
-            logger.warn(String.format("class %s not method %s! please check cofig!", tKey, e.getMessage()));
         } catch (InvocationTargetException | IllegalAccessException e) {
             logger.error(e.getMessage(), e);
         }
@@ -143,4 +110,33 @@ public interface EntityObjectUtils {
             return null;
         }
     }
+
+    private static Object getValue(AccessibleObject val, Object target) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        EntityObjectKey entityObjectKey;
+        Object value;
+        if (val instanceof Method) {
+            Method method = ((Method) val);
+            entityObjectKey = val.getAnnotation(EntityObjectKey.class);
+            value = method.invoke(target);
+        } else {
+            Field field = ((Field) val);
+            entityObjectKey = field.getAnnotation(EntityObjectKey.class);
+            value = field.get(target);
+        }
+        if (entityObjectKey != null) {
+            Class<? extends EntitySpecialType> specialTypeClass = entityObjectKey.specialType();
+            if (specialTypeClass.equals(EntitySpecialType.class)) {
+                return value;
+            } else {
+                EntitySpecialType specialType = ApplicationContextProvider.getBean(specialTypeClass);
+                return specialType.getValue(value);
+            }
+        }
+        return value;
+    }
+}
+
+class EntityObjectDeclare {
+    AccessibleObject[] annotatoionAccessibleObjects;
+    Method[] getMethod;
 }
