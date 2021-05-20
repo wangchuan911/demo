75122b4e8e918be2c0aa0f02a8a81fd83fb8cfe4
Wang ZhiDong <6027021846@iwhalecloud.com>
2021-05-20 11:05:28 +0800

    4.0.2

---
+++ common-utils/src/main/java/org/welisdoon/common/ObjectUtils.java
@@ -0,0 +1,62 @@
+package org.welisdoon.common;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.*;
+
+/**
+ * @Classname ObjectUtils
+ * @Description TODO
+ * @Author wang.zhidong
+ * @Date 2021/4/23 10:17
+ */
+public class ObjectUtils {
+    @FunctionalInterface
+    public interface newObjectFunction<T> {
+        T create();
+    }
+
+    public static <K, V> V getMapValueOrNew(Map<K, V> map, K key, newObjectFunction<V> function) {
+        if (!map.containsKey(key)) {
+            synchronized (map) {
+                if (!map.containsKey(key)) {
+                    V v = function.create();
+                    map.put(key, v);
+                }
+            }
+        }
+        return map.get(key);
+    }
+
+    public static Class<?>[] getGenericTypes(Class<?> type) {
+        Type superClass = type.getGenericSuperclass();
+        Type[] t = ((ParameterizedType) superClass).getActualTypeArguments();
+        return Arrays.stream(t).map(type1 -> {
+            if (type1 instanceof ParameterizedType) {
+                return (Class<?>) ((ParameterizedType) type1).getRawType();
+            } else if (type1 instanceof Class) {
+                return (Class<?>) type1;
+            } else {
+                System.out.println(type1);
+                return null;
+            }
+        }).toArray(Class[]::new);
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Map map = new HashMap<String, Integer>();
+        /*final int[] j = {0};
+        int[] i=new int[1000];
+        Arrays.stream(i).parallel().forEach(value -> {
+            int jj = j[0]++;
+            System.out.println(Thread.currentThread().getName());
+            System.out.println(jj);
+        });
+        Thread.sleep(5000);
+        System.out.println(map);
+        System.out.println(map.size());*/
+        System.out.println(Arrays.toString(getGenericTypes(map.getClass())));
+        ;
+    }
+}
