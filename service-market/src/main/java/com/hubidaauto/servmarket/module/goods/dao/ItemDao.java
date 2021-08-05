0b0051f94d052f3958bb4dfe94d6a34a88af9c1b
wangchuan911 <septem2005@126.com>
2021-08-05 22:39:37 +0800

    4.0.2

--- service-market/src/main/java/com/hubidaauto/servmarket/module/goods/dao/ItemDao.java
+++ service-market/src/main/java/com/hubidaauto/servmarket/module/goods/dao/ItemDao.java
@@ -1,9 +1,12 @@
 package com.hubidaauto.servmarket.module.goods.dao;
 
+import com.baomidou.dynamic.datasource.annotation.DS;
 import com.hubidaauto.servmarket.module.goods.entity.ItemCondition;
 import com.hubidaauto.servmarket.module.goods.entity.ItemVO;
 import org.springframework.stereotype.Repository;
 import org.welisdoon.web.common.dao.ITemplateDao;
+
 @Repository
+@DS("shop")
 public interface ItemDao extends ITemplateDao<Long, ItemVO, ItemCondition> {
 }
--- service-market/src/main/java/com/hubidaauto/servmarket/module/goods/dao/ItemTypeDao.java
+++ service-market/src/main/java/com/hubidaauto/servmarket/module/goods/dao/ItemTypeDao.java
@@ -1,5 +1,6 @@
 package com.hubidaauto.servmarket.module.goods.dao;
 
+import com.baomidou.dynamic.datasource.annotation.DS;
 import com.hubidaauto.servmarket.module.goods.entity.ItemCondition;
 import com.hubidaauto.servmarket.module.goods.entity.ItemTypeCondition;
 import com.hubidaauto.servmarket.module.goods.entity.ItemTypeVO;
@@ -8,5 +9,6 @@ import org.springframework.stereotype.Repository;
 import org.welisdoon.web.common.dao.ITemplateDao;
 
 @Repository
+@DS("shop")
 public interface ItemTypeDao extends ITemplateDao<Long, ItemTypeVO, ItemTypeCondition> {
 }
