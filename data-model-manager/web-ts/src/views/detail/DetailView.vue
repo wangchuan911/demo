ad41efc2bd022fe6c89fba61a1f9d151c2734bcc
wangchuan911 <septem2005@126.com>
2023-02-08 11:58:42 +0800

    ts

--- data-model-manager/web-ts/src/views/detail/DetailView.vue
+++ data-model-manager/web-ts/src/views/detail/DetailView.vue
@@ -58,21 +58,52 @@
 
     <template v-for="define in defines " :key="define.code">
       <el-form-item :label="define.name">
-        <el-input v-if="define.type=='default'" v-model="form[define.key]" :placeholder="define.placeholder"/>
-        <el-input v-else-if="define.type=='textarea'" autosize type="textarea" v-model="form[define.key]"
-                  :placeholder="define.placeholder"/>
-        <el-input v-else-if="define.type=='search'"
-                  v-model="form[define.key]"
-                  :placeholder="define.placeholder"
-                  class="input-with-select"
-        >
-          <template #prepend>
-            <el-select v-model="select" placeholder="Select" style="width: 115px">
-              <el-option label="Restaurant" value="1"/>
-              <el-option label="Order No." value="2"/>
-              <el-option label="Tel" value="3"/>
+        <template v-if="define.mode=='input'">
+          <el-input
+              v-if="['color','date','datetime','email','month','number','password','search','tel','text','time','url','week'].indexOf(define.type)>=0"
+              :type="define.type" v-model="form[define.key]"
+              :placeholder="define.placeholder"/>
+          <template v-else-if="define.type=='checkbox'">
+            <el-checkbox-group v-if="define.init.length<=5" v-model="form[define.key]">
+              <el-checkbox
+                  :label="init.value" :name="define.key"
+                  v-for=" init in define.init||[]" :key="init.value">{{ init.name }}
+              </el-checkbox>
+            </el-checkbox-group>
+            <el-select v-else
+                       v-model="form[define.key]"
+                       multiple
+                       placeholder="Select"
+                       style="width: 240px"
+            >
+              <el-option
+                  v-for=" init in define.init||[]" :key="init.value"
+                  :label="init.name"
+                  :value="init.value"
+              />
             </el-select>
           </template>
+          <template v-else-if="define.type=='radio'">
+            <el-radio-group v-if="define.init.length<=5" v-model="form[define.key]">
+              <el-radio :label="init.value" size="large"
+                        v-for=" init in define.init||[]" :key="init.value">{{ init.name }}
+              </el-radio>
+            </el-radio-group>
+            <el-select v-else v-model="form[define.key]">
+              <el-option
+                  v-for=" init in define.init||[]" :key="init.value"
+                  :label="init.name"
+                  :value="init.value"
+                  :disabled="init.disabled||false"
+              />
+            </el-select>
+          </template>
+        </template>
+        <el-input v-else-if="define.mode==='custom'"
+                  v-model="form[define.key]"
+                  :placeholder="define.placeholder"
+                  class="input-with-select" readonly
+        >
           <template #append>
             <el-button :icon="Search"/>
           </template>
@@ -86,14 +117,54 @@
 
 <script lang="ts" setup>
 import {ref, reactive} from 'vue'
-import { Search } from '@element-plus/icons-vue'
+import {Search} from '@element-plus/icons-vue'
 
 // do not use same name with ref
 const defines = reactive([
-  {name: "Activity 1", type: "default", key: "name1"},
-  {name: "Activity 2", type: "search", key: "name2"},
-  {name: "Activity 3", type: "textarea", key: "name3"},
-  {name: "Activity 4", type: "default", key: "name4"}
+  //{name: "Activity button", type: "button", key: "name4", mode: "input"},
+  //{name: "Activity checkbox", type: "checkbox", key: "name4", mode: "input"},
+  {name: "Activity color", type: "color", key: "name4", mode: "input"},
+  {name: "Activity date", type: "date", key: "name4", mode: "input"},
+  {name: "Activity datetime-local", type: "datetime-local", key: "name4", mode: "input"},
+  {name: "Activity email", type: "email", key: "name4", mode: "input"},
+  //{name: "Activity file", type: "file", key: "name4", mode: "input"},
+  // {name: "Activity hidden", type: "hidden", key: "name4", mode: "input"},
+  //{name: "Activity image", type: "image", key: "name4", mode: "input"},
+  {name: "Activity month", type: "month", key: "name4", mode: "input"},
+  {name: "Activity number", type: "number", key: "name4", mode: "input"},
+  {name: "Activity password", type: "password", key: "name4", mode: "input"},
+  //{name: "Activity radio", type: "radio", key: "name4", mode: "input"},
+  {name: "Activity range", type: "range", key: "name4", mode: "input"},
+  //{name: "Activity reset", type: "reset", key: "name4", mode: "input"},
+  {name: "Activity search", type: "search", key: "name4", mode: "input"},
+  //{name: "Activity submit", type: "submit", key: "name4", mode: "input"},
+  {name: "Activity tel", type: "tel", key: "name4", mode: "input"},
+  {name: "Activity text", type: "text", key: "name4", mode: "input"},
+  {name: "Activity time", type: "time", key: "name4", mode: "input"},
+  {name: "Activity url", type: "url", key: "name4", mode: "input"},
+  {name: "Activity week", type: "week", key: "name4", mode: "input"},
+  {
+    name: "Activity checkbox", type: "checkbox", key: "name5", mode: "input",
+    init: [
+      {name: "Online activities", value: 1},
+      {name: "Online activities2", value: 2},
+      {name: "Offline activities", value: 3},
+      {name: "Simple brand exposure", value: 4},
+      {name: "Simple brand exposure1", value: 5},
+      {name: "Simple brand exposure2", value: 6}]
+  },
+  {
+    name: "Activity radio", type: "radio", key: "name6", mode: "input",
+    init: [
+      {name: "Online activities", value: 1},
+      {name: "Online activities2", value: 2},
+      {name: "Offline activities", value: 3},
+      {name: "Simple brand exposure", value: 4},
+      {name: "Simple brand exposure1", value: 5},
+      {name: "Simple brand exposure2", value: 6}]
+  },
+
+  // {name: "Activity datetime", type: "datetime", key: "name4", mode: "input"}
 ])
 const form = reactive({
   name: 'qwe',
