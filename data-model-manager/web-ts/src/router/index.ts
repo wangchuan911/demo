992044576b33cdf81fea8ece1faa0033bf34def5
wangchuan911 <septem2005@126.com>
2023-02-08 18:51:22 +0800

    ts

--- data-model-manager/web-ts/src/router/index.ts
+++ data-model-manager/web-ts/src/router/index.ts
@@ -24,6 +24,11 @@ const routes: Array<RouteRecordRaw> = [
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "about" */ '../views/detail/DetailView.vue')
+    },
+    {
+        path: '/index',
+        name: 'index',
+        component: () => import(/* webpackChunkName: "about" */ '../views/MainIndex.vue')
     }
 ]
 
---
+++ data-model-manager/web-ts/src/views/MainIndex.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-container class="container">
+    <el-header class="header">Header</el-header>
+    <el-container style="width: 100%;height: 100%">
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>
+        <el-tabs type="border-card" style="height: calc(100vh - 110px)">
+          <el-tab-pane label="User">User</el-tab-pane>
+          <el-tab-pane label="Config">Config</el-tab-pane>
+          <el-tab-pane label="Role">Role</el-tab-pane>
+          <el-tab-pane label="Task">Task</el-tab-pane>
+        </el-tabs>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script setup>
+// eslint-disable-next-line vue/no-export-in-script-setup
+
+</script>
+
+<style scoped>
+.container {
+  width: calc(100vw);
+  height: calc(100vh);
+  background-color: cornflowerblue;
+}
+</style>
