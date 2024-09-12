b8fe3802c25efed49b76cdeb21e383d615edbe2a
Septem <septem2005@126.com>
2024-09-13 00:45:52 +0800

    md object query

---
+++ data-model-manager/web-ts/src/components/form/input/MySelect.vue
@@ -0,0 +1,40 @@
+<template>
+  <el-select v-bind="propsProp" v-model="model">
+    <el-option
+        v-for="item in propsOption"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+    />
+  </el-select>
+</template>
+
+<script lang="ts" setup>
+import {
+  ref,
+  reactive,
+  getCurrentInstance,
+  ComponentInternalInstance,
+  defineProps,
+  defineEmits,
+  watch,
+  computed,
+  defineModel, ComponentCustomProperties
+} from 'vue';
+
+import {ElMessageBox, ElMessage} from 'element-plus';
+import type {Action} from 'element-plus';
+import 'element-plus/es/components/select/style/css';
+import 'element-plus/es/components/option/style/css';
+
+const model = defineModel();
+const props = defineProps<{ option: Array<{ label: string, value: any }>, prop: any }>();
+const propsOption = computed(() => props.option);
+const propsProp = computed(() => props.prop);
+
+
+</script>
+
+<style scoped>
+
+</style>
